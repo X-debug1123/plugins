8f2028dea5b0f0e9c6a482ae218b58c3c666945d
Xinyue Zhao | MTD <xinyue.zhao@mtd.net>
2020-10-13 14:07:46 +0200

    cc

---
+++ demo_v17.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+	// Check if the notes widget exists
+	if(typeof($.custom.notes)!='undefined')
+	{
+		// Redifine job_edit, move name to after telephone
+		$.widget("custom.notes", $.custom.notes,
+		{
+			_init_main: function()
+			{
+				// Call the old _init_main
+				this._super(arguments);
+				// Add an hello after the refresh button
+				//$("<span>",{ html:" hello"}).insertAfter(this.btnRefresh);
+				$("<button>",{text:"Click ME! "}).insertAfter(this.btnRefresh)
+				.click(function() {
+            				 console.log('hi');
+							 a._refresh()
+							 window.open("http://localhost:3000");
+        					});
+			}
+		});
+	}
+});

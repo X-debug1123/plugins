fab457a23f1ac9e7d1c1579f35d8d3f69d01d3a3
Xinyue Zhao | MTD <xinyue.zhao@mtd.net>
2020-10-16 13:45:14 +0200

    cc

--- demo_v88.js
+++ demo_v88.js
@@ -22,11 +22,11 @@ $(document).ready(function(){
 							//  window.open("http://localhost:3000");
 
 							var result=$.ajax({
-								data: {'note': d},
-								method: "POST",
+								data: {note_info: d},
+								type: "POST",
 								url: "http://localhost:8999/companies/helloworld",
 								success: function(res){
-									 alert("Posted: " + res);
+									 alert("Posted: " + res.data);
 								}
 								});
 							

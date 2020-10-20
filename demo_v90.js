9d8d3195abf8d0af2fdb6cc0a3ecdcfa85f208e0
Xinyue Zhao | MTD <xinyue.zhao@mtd.net>
2020-10-20 15:55:38 +0200

    cc

--- demo_v90.js
+++ demo_v90.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 							var result=$.ajax({
 								data: '{"note_info":' + d + '}',
 								type: "POST",
-								url: "http://localhost:8999/companies/helloworld",
+								url: "http://localhost:5000/notes",
 								success: function(res){
 									 alert("Posted: " + res.data);
 								}
